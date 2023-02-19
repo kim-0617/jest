6d7ba387fa3c1db7bee1a7c4949db3627ccf746a
kim-0617 <kkk5993@naver.com>
2023-02-19 22:53:43 +0900

    hello 테스트를 위한 user 추가와 타이머 추가

--- react_test/src/App.js
+++ react_test/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Hello from "./components/Hello";
+import Timer from "./components/Timer";
+
+const user = {
+  name: "Mike",
+  age: 30,
+  gender: "male",
+};
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      {/* <Hello user={user} /> */}
+      <Timer />
+    </>
+  );
 }
 
 export default App;
