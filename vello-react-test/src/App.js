90cdeb84c126d523fa5c13270789b7a43e9b2184
kim-0617 <kkk5993@naver.com>
2023-02-21 14:48:54 +0900

    Delayed Toggle 추가 비동기 학습

--- vello-react-test/src/App.js
+++ vello-react-test/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Counter from "./components/Counter";
 import Profile from "./components/Profile";
+import Counter from "./components/Counter";
+import DelayedToggle from "./components/DelayedToggle";
 
 const App = () => {
   // return <Profile username="velopert" name="김민준" />;
-  return <Counter />;
+  // return <Counter />;
+  return <DelayedToggle />;
 };
 
 export default App;
