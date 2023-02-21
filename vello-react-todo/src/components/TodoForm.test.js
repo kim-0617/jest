ada7a70f6bf39da61950a5380c05a89f2ad33736
kim-0617 <kkk5993@naver.com>
2023-02-21 11:23:00 +0900

    getByText가 잘 되는지 테스트

--- vello-react-todo/src/components/TodoForm.test.js
+++ vello-react-todo/src/components/TodoForm.test.js
@@ -21,6 +21,12 @@ describe("<TodoForm />", () => {
     expect(button).toBeTruthy();
   });
 
+  it("has input and a button", () => {
+    const { getByText, getByPlaceholderText } = render(<TodoForm />);
+    getByPlaceholderText("할 일을 입력하세요"); // input 이 있는지 확인
+    getByText("등록"); // button이 있는지 확인
+  });
+
   it("changes input", () => {
     const { input } = setUp();
 
