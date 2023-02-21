541e02976cee7207e1a0109a091f65794198cdc1
kim-0617 <kkk5993@naver.com>
2023-02-21 17:37:14 +0900

    잠깐 테스트 :art:

--- vello-react-test/src/App.js
+++ vello-react-test/src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Profile from "./components/Profile";
 import Counter from "./components/Counter";
 import DelayedToggle from "./components/DelayedToggle";
 import UserProfile from "./components/UserProfile";
+import axios from "axios";
 
 const App = () => {
+  const [data, setData] = useState(null);
+
   // return <Profile username="velopert" name="김민준" />;
   // return <Counter />;
   // return <DelayedToggle />;
-  return <UserProfile id={1} />;
+  // return <UserProfile id={1} />;
+
+  <>{data}</>;
 };
 
 export default App;
