c444585e55a9dc9ea557a86df2410ee7bf1a2b32
kim-0617 <kkk5993@naver.com>
2023-02-21 15:14:43 +0900

    rest api test

--- vello-react-test/src/App.js
+++ vello-react-test/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Profile from "./components/Profile";
 import Counter from "./components/Counter";
 import DelayedToggle from "./components/DelayedToggle";
+import UserProfile from "./components/UserProfile";
 
 const App = () => {
   // return <Profile username="velopert" name="김민준" />;
   // return <Counter />;
-  return <DelayedToggle />;
+  // return <DelayedToggle />;
+  return <UserProfile id={1} />;
 };
 
 export default App;
